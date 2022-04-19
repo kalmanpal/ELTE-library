91f3629897df70bcac91f1705cde4572a72c5e63
kalmanpal <kalmanpal98@gamil.com>
2022-04-19 23:34:15 +0200

    my subs display update

--- resources/views/member/subscriptions.blade.php
+++ resources/views/member/subscriptions.blade.php
@@ -11,15 +11,18 @@
             <div class="card">
                 <div class="card-header">Előfizetéseim</div>
                 <div class="card-body">
-
                     <h6>Összes hónap: {{$allSubs[0]->all_months}}</h6>
-                    <h6>Státusz: Aktív</h6>
-                    <p></p>
-                    <h6>Kedvezmények: ---------</h6>
-                    <h6>Fizetnivalók: ---------</h6>
+                    @if ($allSubs[0]->active === 1)
+                        <h6>Előfizetés: Aktív, eddig: {{$allSubs[0]->subexpiry}}</h6>
+                    @else
+                        <h6>Előfizetés: Inaktív</h6>
+                    @endif
+
                     <p></p>
                     <table class="table">
-
+                        <h6>Korábbi előfizetések</h6>
+                        <th>Kezdés</th>
+                        <th>Lejárat</th>
                         <tbody>
                             @foreach ($oldSubs as $item)
                         <tr>
