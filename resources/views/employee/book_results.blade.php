12605d073937d86fe1d4328a87ea637d7eb9da2b
kalmanpal <kalmanpal98@gamil.com>
2022-04-29 13:51:12 +0200

    updated views on searc results

--- resources/views/employee/book_results.blade.php
+++ resources/views/employee/book_results.blade.php
@@ -9,23 +9,27 @@
                     Keresés találatok
                 </div>
                 <div class="card-body">
-                        <div class="webshop-container">
-                            @foreach ($booksSearchedByEmp as $item)
-                                <a class="no-underline" href="{{ url('edit-book/'.$item->id) }}">
-                                    <div class="card" style="width: 15rem;">
-                                        <img class="card-img-top book-image" src="{{ asset($item->picture) }}" alt="">
-                                        <div class="card-body" style="height: 195px">
-                                            <h5 class="card-title">{{ $item->title }}</h5>
-                                            <p class="card-text card-remove-gap">{{ $item->isbn }}</p>
-                                            <p class="card-text card-remove-gap">{{ $item->writer }}</p>
-                                            <p class="card-text card-remove-gap">{{ $item->release }}</p>
-                                            <p class="card-text card-remove-gap">Összes: {{ $item->max_number }}</p>
-                                            <p class="card-text card-remove-gap">Elérhető: {{ $item->available_number }}</p>
+                        @if ($booksSearchedByEmp->isEmpty())
+                            <div>Nincs Találat.</div>
+                        @else
+                            <div class="webshop-container">
+                                @foreach ($booksSearchedByEmp as $item)
+                                    <a class="no-underline" href="{{ url('edit-book/'.$item->id) }}">
+                                        <div class="card" style="width: 15rem;">
+                                            <img class="card-img-top book-image" src="{{ asset($item->picture) }}" alt="">
+                                            <div class="card-body" style="height: 195px">
+                                                <h5 class="card-title">{{ $item->title }}</h5>
+                                                <p class="card-text card-remove-gap">{{ $item->isbn }}</p>
+                                                <p class="card-text card-remove-gap">{{ $item->writer }}</p>
+                                                <p class="card-text card-remove-gap">{{ $item->release }}</p>
+                                                <p class="card-text card-remove-gap">Összes: {{ $item->max_number }}</p>
+                                                <p class="card-text card-remove-gap">Elérhető: {{ $item->available_number }}</p>
+                                            </div>
                                         </div>
-                                  </div>
-                                </a>
-                            @endforeach
-                        </div>
+                                    </a>
+                                @endforeach
+                            </div>
+                        @endif
                 </div>
             </div>
         </div>
--- resources/views/employee/user_results.blade.php
+++ resources/views/employee/user_results.blade.php
@@ -7,29 +7,32 @@
             <div class="card">
                 <div class="card-header">Keresés találatok</div>
                 <div class="card-body">
-
-                    <table class="table">
-                        <tbody>
-                            @foreach ($usersSearched as $item)
-                                <tr id="{{ $item->id }}">
-                                    <td>
-                                    <a class="td_class" href="{{ url('member/'.$item->id) }}">{{ $item->name }}</a>
-                                    </td>
-                                    <td>
-                                        {{ $item->email }}
-                                    </td>
-                                    <td>
-                                        {{ $item->city }}
-                                    </td>
-                                    <td>
-                                        {{ $item->address }}
-                                    </td>
-                                    <td>
-                                        {{ $item->type }}
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
+                    @if ($usersSearched->isEmpty())
+                        <div>Nincs Találat.</div>
+                    @else
+                        <table class="table">
+                            <tbody>
+                                @foreach ($usersSearched as $item)
+                                    <tr id="{{ $item->id }}">
+                                        <td>
+                                            <a class="td_class" href="{{ url('member/'.$item->id) }}">{{ $item->name }}</a>
+                                        </td>
+                                        <td>
+                                            {{ $item->email }}
+                                        </td>
+                                        <td>
+                                            {{ $item->city }}
+                                        </td>
+                                        <td>
+                                            {{ $item->address }}
+                                        </td>
+                                        <td>
+                                            {{ $item->type }}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                    @endif
                 </div>
             </div>
         </div>
