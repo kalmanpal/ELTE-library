0e7c6478e64c29c8668bb6dc932370d58b4cbd8e
kalmanpal <kalmanpal98@gamil.com>
2022-05-28 11:04:01 +0200

    -

--- resources/views/mail/nb.blade.php
+++ resources/views/mail/nb.blade.php
@@ -1,3 +1,8 @@
 <h2>Kedves Olvasónk!</h2>
 <p>Ezúton értesítjük, hogy új könyv érkezett könyvtárunkba. Mostantól elérhető a {{$mail_data['release']}}-es megjelenésű {{$mail_data['title']}} , {{$mail_data['author']}}-tól. </p>
 <p>Kellemes olvasást kívánunk!</p>
+<p>
+    <a href="konyvtar2022.szakdolgozat.net">
+        konyvtar2022.szakdolgozat.net
+    </a>
+</p>
--- resources/views/mail/pw.blade.php
+++ resources/views/mail/pw.blade.php
@@ -1,3 +1,8 @@
 <h2>Üdvözöljök az ELTE könyvtárában {{$mail_data['name']}}!</h2>
 <p>A {{$mail_data['email']}} címhez tartozó ELTE könyvtári fiókjába a(z) {{$mail_data['password']}} jelszóval tud majd bejelentkezni. Jelszavát ne ossza meg senkivel, és mielőbb változtassa meg!</p>
 <p>Kellemes olvasást kívánunk!</p>
+<p>
+    <a href="konyvtar2022.szakdolgozat.net">
+        konyvtar2022.szakdolgozat.net
+    </a>
+</p>
